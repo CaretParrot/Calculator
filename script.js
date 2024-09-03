a108910e2fab45899f966bfceb489533f2f32e1d
CaretParrot <wopow29@gmail.com>
2024-09-02 21:28:56 -0500

    Added rounding to numbers

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+window.onload = function () {
+    mathPlus.settings.rounding = 10;
+}
+
 document.getElementById("input").oninput = function (event) {
     let newOutput = Function(`return ${document.getElementById("input").value}`)();
     if (newOutput === undefined || newOutput === null) {
         document.getElementById("output").innerHTML = "...";
     } else {
-        document.getElementById("output").innerHTML = Function(`return ${document.getElementById("input").value}`)();
+        document.getElementById("output").innerHTML = mathPlus.roundToPlaces(newOutput);
     }
 }
