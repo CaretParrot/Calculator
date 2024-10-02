ca1cdfa2aca7a7f0c0bd7c36f7479140ad72bd43
CP-Rishi <161362123+CP-Rishi@users.noreply.github.com>
2024-10-02 12:33:31 -0500

    Added unary operators to force number to become a number

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ document.getElementById("input").focus();
 
 function evaluateInput() {
     let newOutput = Function(`"use strict"; return ${document.getElementById("input").value};`)();
-    if (newOutput === undefined || newOutput === null || regExp.test(newOutput)) {
+    if (+newOutput === undefined || +newOutput === null) {
         document.getElementById("output").innerHTML = "...";
     } else {
-        document.getElementById("output").innerHTML = `${mathPlus.roundToPlaces(newOutput)}`;
+        document.getElementById("output").innerHTML = `${mathPlus.roundToPlaces(+newOutput)}`;
     }
 }
 
